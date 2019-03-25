703e09f9751d9e262a2da621cfb4e4843bf364fe
ruchir_sachdeva <ruchirsachdeva@gmail.com>
2019-03-25 16:01:46 +0100

    enum deserialization error fix

--- src/main/java/com/lnu/foundation/model/OrganizationType.java
+++ src/main/java/com/lnu/foundation/model/OrganizationType.java
@@ -1,5 +1,10 @@
 package com.lnu.foundation.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public enum OrganizationType {
     VENUE,FOOD,DECOR,GROOM
 }
